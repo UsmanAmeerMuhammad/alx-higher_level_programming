f7a47f5e0a6194c7bf06eacef4ac4342ca541330
UsmanAmeerMuhammad <ameerusman091@gmail.com>
2024-01-21 12:29:22 -0800

    Script concatenates edges

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\languaage that combiness remarkable power with very clear syntax"
+str = str[38:66] + " with Python"
+print(str)
