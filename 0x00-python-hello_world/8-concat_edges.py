bae75c93db141b727ad7d69b2465abe40c9e4e64
UsmanAmeerMuhammad <ameerusman091@gmail.com>
2024-01-21 12:31:42 -0800

    fixed pycodestyle error

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\languaage that combiness remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 str = str[38:66] + " with Python"
 print(str)
