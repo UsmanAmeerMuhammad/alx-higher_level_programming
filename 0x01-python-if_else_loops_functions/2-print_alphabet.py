d18f08387fdc74f94cb3bea8954e1f5f0f958a48
UsmanAmeerMuhammad <ameerusman091@gmail.com>
2024-01-23 05:40:32 -0800

    fixed error

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 alpha = 'a'
-for alpha in range(ord('a'), ord('z')):
-    print(chr(alpha), end=' ')
+for alpha in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(alpha), end=' ')
